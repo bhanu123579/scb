a4d4740f0c60891b84ca2dc38d7930a9e40c3e0d
bhanuprakash13579 <bhanuprakashnaidu13579@gmail.com>
2025-05-01 23:06:33 +0530

    updated urls.py

--- netbanking/urls.py
+++ netbanking/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='home'),  # this is the homepage for the app
-    path('online.html', views.online, name='online'),
+    path('login.html', views.online, name='online'),
 path('otp_page.html', views.otp_page, name='otp_page'),
 path('landing.html', views.landing, name='landing'),
 path('estatement_page.html', views.estatement_page, name='estatement_page'),
